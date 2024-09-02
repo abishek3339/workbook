733b409bdbb50b8b2c3bce747b89fd5f56fe4361
abishek km <abishekkm12@gmail.com>
2024-09-02 09:10:42 +0530

    initial file

---
+++ assignmentoperator/src/assignmentoperatorpkg/Demoassignmnt.java
@@ -0,0 +1,21 @@
+package assignmentoperatorpkg;
+
+public class Demoassignmnt {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		/*int x=5;
+				x+=3;
+				System.out.println(x);*/
+		/*int y=5;
+		y-=2;
+				System.out.println(y);*/
+		/*int m=10;
+		m*=2;
+				System.out.println(m);*/
+		
+
+	}
+
+}
